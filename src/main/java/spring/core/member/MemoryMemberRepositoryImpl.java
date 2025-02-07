a802888f0c9f2aa351b104068ddda8fdcef666e3
leeejunu <leeejunu@gmail.com>
2025-02-07 18:29:21 +0900

    add lombok

--- src/main/java/spring/core/member/MemoryMemberRepositoryImpl.java
+++ src/main/java/spring/core/member/MemoryMemberRepositoryImpl.java
@@ -1,7 +1,6 @@
 package spring.core.member;
 
-import lombok.Getter;
-import org.springframework.context.annotation.ComponentScan;
+
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
